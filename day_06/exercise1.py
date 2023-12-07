8c90b6c8a91b69f4c4933618e08e410ab6e5891d
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2023-12-07 23:07:08 +0100

    day 6 exercise 2

--- day_06/exercise1.py
+++ day_06/exercise1.py
@@ -13,6 +13,36 @@ family_members = list(siblings)
 family_members.append("Abdullahi")
 family_members.append("Mother")
 
-family_members = tuple(family_members)
-
 print(family_members)
+
+#exercise 2
+
+siblings, parents = family_members[:-2], family_members[-2:]
+
+print(siblings)
+print(parents)
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+vegetables = ('Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot')
+animal_products = ('milk', 'meat', 'butter', 'yoghurt')
+food_stuff_tp = fruits + vegetables + animal_products
+
+print(food_stuff_tp)
+food_stuff_lt = list(food_stuff_tp)
+print(food_stuff_lt)
+
+import math
+
+middle_item = food_stuff_lt[math.floor(len(food_stuff_lt)/2)]
+print(middle_item)
+
+first_three, last_three = food_stuff_lt[:3], food_stuff_lt[-3:]
+print(first_three)
+print(last_three)
+
+del food_stuff_tp
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries)
